2fefa14050645dce08c8fb2972e79ff74ef7cb17
ze25800000 <ze258100000@sina.com>
2017-10-22 21:00:36 +0800

    8-3 商家入驻申请-图片上传处理

--- application/api/controller/Image.php
+++ application/api/controller/Image.php
@@ -1,7 +1,17 @@
 <?php
+
 namespace app\api\controller;
 
-class Image
-{
+use think\Controller;
+use think\Request;
 
+class Image extends Controller {
+    public function upload() {
+        $file = Request::instance()->file('file');
+        $info = $file->move('upload');
+        if ($info && $info->getPathname()) {
+            return show(1, 'success', '/' . $info->getPathname());
+        }
+        return show(0, 'upload error');
+    }
 }
